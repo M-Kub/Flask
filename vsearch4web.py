cf96c277c5d70938ec782207f362c9ad03569e03
michel <mkubesch@gmail.com>
2018-05-20 11:45:54 +0200

    Added Exceptions

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,25 +1,28 @@
 from flask import Flask, render_template, request, escape, session
 from vsearch import search4letters
 
-from DBcm import UseDatabase
+from DBcm import UseDatabase, ConnectionError, SQLError, CredentialsError
 from checker import check_logged_in
 
 app = Flask(__name__)
 
-app.config['dbconfig'] = {  'host': '127.0.0.1',
-                            'user': 'vsearch',
-                            'password': 'michel',
-                            'database': 'vsearchlogDB', }
+app.config['dbconfig'] = {'host': '127.0.0.1',
+                          'user': 'vsearch',
+                          'password': 'michel',
+                          'database': 'vsearchlogDB', }
+
 
 @app.route('/login')
 def do_login() -> str:
     session['logged_in'] = True
     return 'You are now logged in.'
 
+
 @app.route('/logout')
 def do_logout() -> str:
     session.pop('logged_in')
     return 'You are now logged out.'
+
 
 def log_request(req: 'flask_request', res: str) -> None:
     with UseDatabase(app.config['dbconfig']) as cursor:
@@ -28,10 +31,11 @@ def log_request(req: 'flask_request', res: str) -> None:
                 values
                 (%s, %s, %s, %s, %s)"""
         cursor.execute(_SQL, (req.form['phrase'],
-                          req.form['letters'],
-                          req.remote_addr,
-                          req.user_agent.browser,
-                          res, ))
+                              req.form['letters'],
+                              req.remote_addr,
+                              req.user_agent.browser,
+                              res, ))
+
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> str:
@@ -44,33 +48,42 @@ def do_search() -> str:
     except Exception as err:
         print('***** Logging failed with this error:', str(err))
     return render_template('results.html',
-                        the_phrase=phrase,
-                        the_letters=letters,
-                        the_title=title,
-                        the_results=results,)
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_title=title,
+                           the_results=results,)
 
 
 @app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html',
-            the_title='Willkommen zur Web Version von Search4letters!')
+                           the_title='Willkommen zur Web Version von Search4letters!')
 
 
 @app.route('/viewlog')
 @check_logged_in
 def view_the_log() -> 'html':
-    with UseDatabase(app.config['dbconfig']) as cursor:
-        _SQL = """select phrase, letters, ip, browser_string, results
-                  from log"""
-        cursor.execute(_SQL)
-        contents = cursor.fetchall()
-        titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
-        return render_template('viewlog.html',
+    try:
+        with UseDatabase(app.config['dbconfig']) as cursor:
+         _SQL = """select phrase, letters, ip, browser_string, results
+                   from log"""
+         cursor.execute(_SQL)
+         contents = cursor.fetchall()
+         titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
+         return render_template('viewlog.html',
                                 the_title='View Log',
                                 the_row_titles=titles,
                                 the_data=contents,)
-
+    except ConnectionError as err:
+        print('Is your database switched on? Error:', str(err))
+    except CredentialsError as err:
+        print('User-id/Password issues. Error:', str(err))
+    except SQLError as err:
+        print('Is your querry correct? Error:', str(err))
+    except Exception as err:
+        print('Something went wrong:', str(err))
+    return 'Error'
 app.secret_key = 'secret'
 
 if __name__ == '__main__':
